100c51e4ab88f97bc0d1c8fc8dd0e3c5f34930fc
Ugo Arzur <ugo.arzur@morning.com>
2020-02-06 16:30:35 +0100

    working draw

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  const canvas = useRef();
+  const [ user, setUser ] = useState({})
+
+  useEffect(() => {
+    console.log(canvas.current)
+    const ctx = canvas.current.getContext('2d');
+    canvas.current.width = window.innerWidth;
+    canvas.current.height = window.innerHeight;
+    ctx.strokeStyle = '#BADA55';
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.lineWidth = 100;
+
+    let isDrawing = false;
+    let lastX = 0;
+    let lastY = 0;
+
+    function draw(e){
+      if(!isDrawing) return; // stop the func from running when hey are not moused
+      console.log(e)
+      ctx.beginPath();
+      // Start from 0
+      ctx.moveTo(lastX, lastY);
+
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+
+      [lastX, lastY] = [e.offsetX, e.offsetY];
+    }
+    // https://youtu.be/8ZGAzJ0drl0?t=363
+    canvas.current.addEventListener('mousemove', draw)
+    canvas.current.addEventListener('mousedown', (e) => {
+      isDrawing = true;
+      [lastX, lastY] = [e.offsetX, e.offsetY];
+    })
+    canvas.current.addEventListener('mouseup', () => isDrawing = false)
+    canvas.current.addEventListener('mouseout', () => isDrawing = false)
+  }, [])
+
+
+  useEffect(() => {
+    setUser(user)
+  }, [user])
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas
+        ref={canvas}
+        id="draw"
+        width="800"
+        height="600"
+        ></canvas>
     </div>
   );
 }

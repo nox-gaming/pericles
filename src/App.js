73771482558c4c794933624269e2cd1432dbad14
Ugo Arzur <ugo.arzur@morning.com>
2020-02-07 11:27:02 +0100

    linewidth

--- src/App.js
+++ src/App.js
@@ -14,14 +14,15 @@ function App() {
     ctx.strokeStyle = '#BADA55';
     ctx.lineJoin = 'round';
     ctx.lineCap = 'round';
-    ctx.lineWidth = 100;
+    ctx.lineWidth = 50;
 
     let isDrawing = false;
     let lastX = 0;
     let lastY = 0;
     let hue = 0;
+    let direction = true;
 
-    function draw(e){
+    function draw(e, color){
       if(!isDrawing) return; // stop the func from running when hey are not moused
       console.log(e)
       ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
@@ -36,6 +37,14 @@ function App() {
       hue++;
       if(hue > 360) {
         hue = 0
+      }
+      if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1){
+        direction = !direction;
+      }
+      if (direction) {
+        ctx.lineWidth++;
+      } else {
+        ctx.lineWidth--;
       }
     }
     // https://youtu.be/8ZGAzJ0drl0?t=363
